659e591f19c3d49eb643f79393cc3d4888c9abe8
ANAS <anas@ANASs-MacBook-Air.local>
2014-06-21 06:41:00 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '70a027ba5a7dd056ede9b46b28e7d7eb7449aaf62d8890fc284a50cb51715e5171b56bb49f0980ea3bbd0a14e9f23389b5fae05a8b8a4729d214b73a66473978'
